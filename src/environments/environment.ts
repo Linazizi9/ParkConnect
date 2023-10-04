80da7f7d7e626661ef3dca55c983a7a21c903676
lina zizi <zizilina003@gmail.com>
2023-10-04 08:35:29 +0100

    feat: firebaseconfig

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'parkconnect-2ab3d',
+    appId: '1:818410311112:web:0bfee8b88de31f06c304fd',
+    databaseURL: 'https://parkconnect-2ab3d-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'parkconnect-2ab3d.appspot.com',
+    apiKey: 'AIzaSyB_AaOhHcNb1WQdcw9oRoySnIir-gAw-MA',
+    authDomain: 'parkconnect-2ab3d.firebaseapp.com',
+    messagingSenderId: '818410311112',
+  },
+production: false
+};
